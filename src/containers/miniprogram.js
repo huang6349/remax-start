e06e63f5f40699202d6b77d601b9e7c2d25ed333
huang6349 <252683858@qq.com>
2021-01-17 19:15:43 +0800

    :tada: 初次提交

---
+++ src/containers/miniprogram.js
@@ -0,0 +1,7 @@
+import { createContainer } from 'unstated-next';
+
+function useMiniprogram() {}
+
+const Miniprogram = createContainer(useMiniprogram);
+
+export default Miniprogram;
---
+++ src/pages/index/index.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import { View } from 'remax/wechat';
+import './index.css';
+
+export default () => {
+  return <View>remax-start</View>;
+};
